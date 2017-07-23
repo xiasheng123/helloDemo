8089bed339c43f98411001afa386f8e05f676520
fuxiasheng <779713335@qq.com>
2017-07-24 00:43:09 +0800

    Initial commit

---
+++ src/com/diaping/merchant/demo/Hello.java
@@ -0,0 +1,9 @@
+package com.diaping.merchant.demo;
+
+public class Hello {
+
+    public Hello(){
+
+        System.out.println("hehe in Hello");
+    }
+}
---
+++ src/com/diaping/merchant/demo/Main.java
@@ -0,0 +1,10 @@
+package com.diaping.merchant.demo;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("hello");
+        Hello hello = new Hello();
+    }
+}
